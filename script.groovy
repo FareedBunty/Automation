2cccf552e1f223da893512699d93f6738b0ed0f8
FareedBunty <49340160+FareedBunty@users.noreply.github.com>
2024-09-19 03:29:16 +0500

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildAPP()
+{
+  echo "Building The Application"    
+}
+def testAPP()
+{
+  echo "TestingThe Application"    
+}
+def deployAPP()
+{
+  echo "Deploying The Application"
+  echo "Deploying The Version ${param.VERSION}
+}
